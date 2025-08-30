890a91a4042cc5d726093dcaa3719ae671999d20
DarkZero <ruanvsd2024@gmail.com>
2025-08-30 20:05:01 -0300

    Update num-Tracker.py

--- num-Tracker.py
+++ num-Tracker.py
@@ -1,55 +1,45 @@
-'''
-ShadowByte
-
-ALL FOR KNOWLEDGE
-'''
-# importa as bibliotecas
 import phonenumbers
-from phonenumbers import geocoder
+from phonenumbers import geocoder, carrier
 from colorama import init, Fore, Style
 import pyfiglet
 import os
 import time
 import sys
 
-# inicia o colorama
 init()
 
-# função limpar_tela
 def limpar_tela():
-    # apagar tudo no comeco e amostrar a logo num-Tracker
-    os.system("cls")
-    if sys.platform == "linux"":
-       os.system("clear")
-       
-limpar_tela()
+    os.system("cls" if sys.platform == "win32" else "clear")
 
-# criar uma função, e cria um texto grande "num-Tracker"
 def nome_da_ferramenta():
-    logo = "num-Tracker"
-    resultado = pyfiglet.figlet_format(logo)
-    print(resultado)
-    
-nome_da_ferramenta()
+    lg = pyfiglet.figlet_format("TN-LOC")
+    print(Fore.YELLOW + f"{lg}" + Style.RESET_ALL)
 
-# função "numero"
-def numero();
-    # digitar um numero de telefone e se não tiver nada, imprime uma mensagem
-    numero = input(Fore.CYAN + "digite um numero de telefone: " + Style.RESET_ALL)
+def Tracker():
+    numero = input(Fore.CYAN + "Digite um número de telefone (com +código do país): " + Style.RESET_ALL)
     if not numero:
-	    print(Fore.YELLOW + "nenhum número digitado" + Style.RESET_ALL)
-	    exit()
+        print(Fore.YELLOW + "Nenhum número digitado" + Style.RESET_ALL)
+        exit()
 
-    # parsear o numero
     num = phonenumbers.parse(numero)
-    time.sleep(3)
-    
-numero()
+    time.sleep(2)
 
-# função "localizacao"
-def localizacao():
-    # imprime: localizaçao e diz a localização do numero
-    print(Fore.RED+ "\nlocalização:" + Style.RESET_ALL)
-    print(geocoder.description_for_number(num, 'pt', "en", "fr", "es"))
+    print(Fore.RED + "\nInformações do telefone:" + Style.RESET_ALL)
     
-loalizacao()
+    estado = geocoder.description_for_number(num, "pt")
+    pais = phonenumbers.region_code_for_number(num)
+    
+    codigo_postal = num.country_code
+    validacao_num = phonenumbers.is_valid_number(num)
+    
+    validacao_CodigoPostal = phonenumbers.is_possible_number(num)
+    formatacao = phonenumbers.format_number(num, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+    operadora = carrier.name_for_number(num, "pt")
+    
+    print(Fore.BLUE+"\nEstado/Região:", estado, "País:", pais, f"\nOperadora: {operadora}", "\nCodigo postal:", codigo_postal, "\nNumero valido: ", validacao_num, "\nCodigo postal valido:", validacao_CodigoPostal, "\nFormato:", formatacao)
+
+if __name__ == "__main__":
+    limpar_tela()
+    nome_da_ferramenta()
+    Tracker()
+			
